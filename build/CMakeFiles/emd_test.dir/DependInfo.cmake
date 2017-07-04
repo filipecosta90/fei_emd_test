dbda74d0ab9cd1e7a308150cf0005aa88832e1bf
filipecosta90 <filipecosta.90@gmail.com>
2017-07-04 16:09:52 +0100

    1st test

---
+++ build/CMakeFiles/emd_test.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/filipeoliveira/Documents/fei_emd_test/test_emd.cpp" "/Users/filipeoliveira/Documents/fei_emd_test/build/CMakeFiles/emd_test.dir/test_emd.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../.."
+  "/usr/local/Cellar/hdf5/1.10.1/include"
+  "/usr/local/opt/szip/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/emd_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/emd_test.dir/test_emd.cpp.o"
+  "emd_test.pdb"
+  "emd_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/emd_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
